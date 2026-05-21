ed4780c1861cdfff586938840f774e03ff9dd5e7
Abinav Balasubbiramaniam <abibalu9001@gmail.comm>
2026-05-21 21:17:27 +0530

    Configured PostgreSQL for TaxonLens

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': dj_database_url.parse(
+
+        os.environ.get(
+            "DATABASE_URL"
+        )
+
+    )
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
